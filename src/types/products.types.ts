b8b6df07a1eea3ada30aab47198e2ae9ac565770
Hakam-aldeen-Kh <hakamha8@gmail.com>
2024-07-10 11:34:05 +0300

    edit product type

--- src/types/products.types.ts
+++ src/types/products.types.ts
@@ -1,64 +1,4 @@
-type ImageFormat = {
-  name: string;
-  hash: string;
-  ext: string;
-  mime: string;
-  path: string | null;
-  width: number;
-  height: number;
-  size: number;
-  sizeInBytes: number;
-  url: string;
-};
-
-type Cover = {
-  id: number;
-  attributes: {
-    name: string;
-    alternativeText: string | null;
-    caption: string | null;
-    width: number;
-    height: number;
-    formats: {
-      thumbnail: ImageFormat;
-      small?: ImageFormat;
-      medium?: ImageFormat;
-      large?: ImageFormat;
-    };
-    hash: string;
-    ext: string;
-    mime: string;
-    size: number;
-    url: string;
-    previewUrl: string | null;
-    provider: string;
-    provider_metadata: null;
-    createdAt: string;
-    updatedAt: string;
-  };
-};
-
-type Status = {
-  id: number;
-  attributes: {
-    title: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-  };
-};
-
-type Category = {
-  id: number;
-  attributes: {
-    title: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-  };
-};
-
-type Product = {
+export type TProduct = {
   id: number;
   attributes: {
     createdAt: string;
@@ -71,27 +11,62 @@ type Product = {
     discount: number | null;
     rating: number;
     cover: {
-      data: Cover | null;
+      data: {
+        id: number;
+        attributes: {
+          name: string;
+          alternativeText: string | null;
+          caption: string | null;
+          width: number;
+          height: number;
+          formats: {
+            thumbnail: {
+              name: string;
+              hash: string;
+              ext: string;
+              mime: string;
+              path: string | null;
+              width: number;
+              height: number;
+              size: number;
+              sizeInBytes: number;
+              url: string;
+            };
+          };
+          hash: string;
+          ext: string;
+          mime: string;
+          size: number;
+          url: string;
+          previewUrl: string | null;
+          provider: string;
+          provider_metadata: string | null;
+          createdAt: string;
+          updatedAt: string;
+        };
+      };
     };
     category: {
-      data: Category | null;
+      data: {
+        id: number;
+        attributes: {
+          title: string;
+          createdAt: string;
+          updatedAt: string;
+          publishedAt: string;
+        };
+      } | null;
     };
     status: {
-      data: Status | null;
+      data: {
+        id: number;
+        attributes: {
+          title: string;
+          createdAt: string;
+          updatedAt: string;
+          publishedAt: string;
+        };
+      };
     };
   };
-};
-
-type Meta = {
-  pagination: {
-    page: number;
-    pageSize: number;
-    pageCount: number;
-    total: number;
-  };
-};
-
-export type TProduct = {
-  data: Product[];
-  meta: Meta;
 };
