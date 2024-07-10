beb02c5163953457a15a7dc58f6fcf5b6e5c3bec
Hakam-aldeen-Kh <hakamha8@gmail.com>
2024-07-10 11:34:35 +0300

    add gap and padding to brand section

--- src/components/sections/Landing-Sections/BrandsSection.tsx
+++ src/components/sections/Landing-Sections/BrandsSection.tsx
@@ -6,16 +6,16 @@ import versace from "@assets/svg/b1Vector.svg";
 
 function BrandsSection() {
   return (
-    <div className="bg-black h-[15vh] bottom-0 z-10 block ">
-      <div className="container flex flex-wrap justify-between items-center md:flex-nowrap w-full h-full flex-shrink-0 ">
+    <div className="bg-black h-[15vh] bottom-0 z-10 block py-2">
+      <div className="container flex flex-wrap  gap-4 justify-between items-center md:flex-nowrap w-full h-full flex-shrink-0 ">
         <div className="md:h-full items-center grid grid-cols-3 gap-4 md:w-[60%]">
-          <img src={versace} alt="Versace" className="object-contain" />
+          <img src={versace} alt="Versace" className="object-contain mx-auto" />
           <img src={zara} alt="Zara" className="object-contain mx-auto" />
-          <img src={gucci} alt="Gucci" className="object-contain" />
+          <img src={gucci} alt="Gucci" className="object-contain mx-auto" />
         </div>
         <div className="md:h-full items-center grid grid-cols-2 gap-4 md:w-[40%] mx-auto">
-          <img src={prada} alt="Prada" className="object-contain" />
-          <img src={calvin} alt="Calvin Klein" className="object-contain" />
+          <img src={prada} alt="Prada" className="object-contain mx-auto" />
+          <img src={calvin} alt="Calvin Klein" className="object-contain mx-auto" />
         </div>
       </div>
     </div>
