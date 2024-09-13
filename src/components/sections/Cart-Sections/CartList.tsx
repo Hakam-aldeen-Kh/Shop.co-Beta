b04c093528214415860ad0d08bf99fb92c6ab3bb
Hakam-aldeen-Kh <hakamha8@gmail.com>
2024-09-13 14:07:49 +0300

    edit some details UI in cartList

--- src/components/sections/Cart-Sections/CartList.tsx
+++ src/components/sections/Cart-Sections/CartList.tsx
@@ -15,58 +15,66 @@ function CartList() {
       {cartItems.map((item, index) => (
         <div key={item.product.id + item.size}>
           {" "}
-          <div className="flex w-full my-3">
+          <div className="flex w-full my-3 relative">
             <img
               src={`http://localhost:1337${item.product.attributes.cover.data.attributes.url}`}
               alt={item.product.attributes.title}
               className="w-[125px] h-[135px] rounded-[15px]"
             />
             <div className="ml-3 w-full">
-              <h2 className="font-bold capitalize">
+              <h2 className="font-bold capitalize w-[90%]">
                 {item.product.attributes.title}
               </h2>
-              <p className="font-semibold inline-block my-2">Color : </p>
-              <span className="pl-1 capitalize">
+              <p className="font-semibold inline-block my-2 text-[13px] md:text-[15px]">
+                Color :{" "}
+              </p>
+              <span className="pl-1 capitalize text-[13px] md:text-[15px]">
                 {item.product.attributes.color.data.attributes.title}
               </span>
               <br />
-              <p className="font-semibold inline-block mb-2">Size : </p>
-              <span className="pl-1 capitalize">{item.size}</span>
+              <p className="font-semibold inline-block mb-2 text-[13px] md:text-[15px]">
+                Size :{" "}
+              </p>
+              <span className="pl-1 capitalize text-[13px] md:text-[15px]">
+                {item.size}
+              </span>
               <p className="font-semibold text-[20px]">
-                {item.product.attributes.price}$
+                ${item.product.attributes.price}
               </p>
             </div>
-            <div className="flex flex-col justify-between w-[30%]">
-              <img
-                src={Recycle}
-                alt="Recycle"
-                className="w-[20px] self-end hover:cursor-pointer"
-                onClick={() => handleRemove(item)}
-              />
-              <div className="flex self-end ">
-                <button
-                  onClick={() => handleDecrement(item)}
-                  className="py-2 px-4 bg-gray-300 rounded-tl-full rounded-bl-full"
-                >
-                  <i className="fa-solid fa-minus"></i>
-                </button>
-                <span className="py-2 px-4 bg-gray-300 flex items-center justify-center">
-                  {item.quantity}{" "}
-                </span>
-                <button
-                  onClick={() => handleIncrement(item)}
-                  className="py-2 px-4 bg-gray-300 rounded-tr-full rounded-br-full"
-                  disabled={
-                    item.quantity === item.product.attributes.stock ||
-                    calculateRemainingStock(item) <= 0
-                  }
-                >
-                  <i className="fa-solid fa-plus"></i>
-                </button>
-              </div>
-              <div className="mt-2 text-sm text-red-600 text-center font-semibold">
-                Remaining stock: {calculateRemainingStock(item)}
-              </div>
+            <img
+              src={Recycle}
+              alt="Recycle"
+              className="w-[20px] self-end hover:cursor-pointer absolute right-0 top-0"
+              onClick={() => handleRemove(item)}
+            />
+            <div className="mt-10 text-xs text-red-600 text-center md:ml-6  font-semibold absolute right-6 top-9 ">
+              In stock: {calculateRemainingStock(item)}
+            </div>
+            <div className="flex self-end  absolute right-0 bottom-1">
+              <button
+                onClick={() => handleDecrement(item)}
+                className="py-1 px-3 bg-gray-300 rounded-tl-full rounded-bl-full"
+              >
+                <i className="fa-solid fa-minus"></i>
+              </button>
+              <span className="py-1 px-3 bg-gray-300 flex items-center justify-center">
+                {item.quantity}{" "}
+              </span>
+              <button
+                onClick={() => handleIncrement(item)}
+                className={`py-1 px-3 bg-gray-300 rounded-tr-full rounded-br-full ${
+                  item.quantity === item.product.attributes.stock
+                    ? "opacity-50"
+                    : ""
+                }`}
+                disabled={
+                  item.quantity === item.product.attributes.stock ||
+                  calculateRemainingStock(item) <= 0
+                }
+              >
+                <i className="fa-solid fa-plus"></i>
+              </button>
             </div>
           </div>
           {index !== cartItems.length - 1 && (
